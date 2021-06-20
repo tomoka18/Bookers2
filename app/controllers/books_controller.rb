23f9343b6bb56ce62b847cdbb9f0ba207d7f8d12
tomoka18 <gudegude18@gmail.com>
2021-06-20 13:52:00 +0000

    [Update4-10]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,24 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to book_path(@book)
   end
 
   def index
      @books = Book.all
+     @user = current_user
+     @book = Book.new
   end
 
   def show
      @book = Book.find(params[:id])
+     @user = current_user
+     @books = Book.all
+
+
   end
 
   def destroy
@@ -27,12 +30,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :image)
+    params.require(:book).permit(:title, :body)
   end
-
-  def user_params
-    params.require(:user).permit(:name, :profile_image)
-  end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :show]
 
   resources :users, only: [:show, :edit, :update, :index]
 
