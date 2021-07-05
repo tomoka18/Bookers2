3de2b6727d9f01401a36013894f3fe6714114588
tomoka18 <gudegude18@gmail.com>
2021-07-05 12:20:50 +0000

    Finish検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'searches/search'
+  get 'search/search'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get '/searches' => 'searches#search'
 
 
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
